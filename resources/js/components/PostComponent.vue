3031c3c5ec8be6771d35d0f814ad4ab9651cf9ca
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-09-18 00:55:22 +0300

    Видео 13. Директива v-if в vuejs. А также метод filter для массивов js (реальое применение computed в Composition API)

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,21 +1,15 @@
 <template>
     <div><SinglePostComponent /></div>
-    <h1>{{ name }}</h1>
-    <h1>{{ job() }}</h1>
-    <button class="btn btn-primary" @click="sayHello">Say Hello</button>
 
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
                 <th scope="col">Имя</th>
-                <th scope="col">Имя</th>
+                <th scope="col">Возраст</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="person in persons" :key="person.id">
-                <th scope="row">1</th>
-                <td>{{ person.id }}</td>
+            <tr v-for="person in personAgeLess30" :key="person.name">
                 <td>{{ person.name }}</td>
                 <td>{{ person.age }}</td>
             </tr>
@@ -25,21 +19,15 @@
 
 <script setup>
 import SinglePostComponent from "./SinglePostComponent.vue";
-import { reactive, ref } from "vue";
+import { reactive, ref, computed } from "vue";
 
-let name = ref("Sergey");
-
-let persons = ref([
+let persons = reactive([
     { name: "Sergey", age: 20 },
     { name: "Alex", age: 25 },
     { name: "Vlad", age: 30 },
 ]);
 
-function sayHello() {
-    alert("Hello, " + name.value);
-}
-
-function job() {
-    return name.value + " работает программистом";
-}
+const personAgeLess30 = computed(() => {
+    return persons.filter((person) => person.age < 30);
+});
 </script>
