9dac044a8fa2cc87a47938db157ca59c38f9877b
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-09-17 22:51:47 +0300

    Видео 6. Создаем первый компонент Vue. Разбираем файл app.js и меняем его

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>228228228228228228228228228228228228228228228</div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
