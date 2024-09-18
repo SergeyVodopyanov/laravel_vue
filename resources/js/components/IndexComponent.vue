ad43f2800f7a56d4b0b89c5729146fb7e8f110f1
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-09-18 23:01:41 +0300

    Видео 17. Асинхронный CRUD. Обновление персоны. #1 Переключатель на форму

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -10,11 +10,37 @@
             </thead>
 
             <tbody>
-                <tr v-for="person in people" :key="person.id">
-                    <th scope="row">1</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                </tr>
+                <template v-for="person in people" :key="person.id">
+                    <tr>
+                        <th scope="row">{{ person.id }}</th>
+                        <td>{{ person.name }}</td>
+                        <td>{{ person.age }}</td>
+                        <td>
+                            <a
+                                href="#"
+                                class="btn btn-success"
+                                @click.prevent="editPerson(person.id)"
+                            >
+                                Редактировать
+                            </a>
+                        </td>
+                    </tr>
+                    <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                        <th scope="row">{{ person.id }}</th>
+                        <td><input type="text" class="form-control" /></td>
+                        <td><input type="number" class="form-control" /></td>
+
+                        <td>
+                            <a
+                                href="#"
+                                class="btn btn-success"
+                                @click.prevent="editPerson(null)"
+                            >
+                                Сохранить
+                            </a>
+                        </td>
+                    </tr>
+                </template>
             </tbody>
         </table>
     </div>
@@ -25,11 +51,19 @@ import axios from "axios";
 import { ref } from "vue";
 
 let people = ref([]);
-
+let editPersonId = ref(null);
 getPeople();
 
 async function getPeople() {
     const response = await axios.get("/api/people");
     people.value = response.data;
+}
+
+function editPerson(id) {
+    editPersonId.value = id;
+}
+
+function isEdit(id) {
+    return editPersonId.value === id;
 }
 </script>
