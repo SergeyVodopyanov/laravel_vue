63fbb783d0640e6fb84bd66bab19676c5fc4572e
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-09-22 23:52:08 +0300

    Видео 20. Функция из д.э. вызывается в р.э. c помощью onMounted

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,22 +2,21 @@
     <div>
         <CreateComponent />
         <IndexComponent ref="indexComponentRef" />
-        <button @click="callIndexLog">Вызвать indexLog</button>
     </div>
 </template>
 
 <script setup>
 import IndexComponent from "./IndexComponent.vue";
 import CreateComponent from "./CreateComponent.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 // Ссылка на компонент IndexComponent
 const indexComponentRef = ref(null);
 
-// Функция для вызова indexLog из IndexComponent
-function callIndexLog() {
+// Вызов функции indexLog при монтировании компонента
+onMounted(() => {
     if (indexComponentRef.value) {
         indexComponentRef.value.indexLog(); // Вызов функции indexLog
     }
-}
+});
 </script>
