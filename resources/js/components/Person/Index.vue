15c36475cd403450b6798daccc618846ed366b28
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-10-07 22:29:56 +0300

    Видео 27. CRUD в SPA приложении. Получение списка персон

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,3 +1,35 @@
 <template>
-    <div>Index</div>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Возраст</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in people" :key="person.id">
+                    <th scope="row">{{ person.id }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+let people = ref([]);
+
+getPeople();
+
+async function getPeople() {
+    axios.get("/api/people").then((res) => {
+        people.value = res.data;
+    });
+}
+</script>
